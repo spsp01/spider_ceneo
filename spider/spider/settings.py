30a14aa5ed9483fae65a82a966250358bfd8bcd7
Jarek <spsp01@o2.pl>
2019-01-11 12:36:44 +0100

    Removed generated comments

--- spider/spider/settings.py
+++ spider/spider/settings.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for spid project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = 'spider'
 
 SPIDER_MODULES = ['spider.spiders']
